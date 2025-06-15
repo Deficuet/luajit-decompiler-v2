6a85260a11438d34d82f3e3848d34e43c80efc59
Deficuet <Deficuet457@gmail.com>
2025-06-15 17:40:41 -0600

    add compatibility for ljd.h to C

--- ljd.h
+++ ljd.h
@@ -2,8 +2,12 @@
 
 #define EXPORT __declspec(dllexport)
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     EXPORT void ljd_file_to_file(const wchar_t *input, const wchar_t *output);
     EXPORT void ljd_bytes_to_file(const wchar_t *name, const char *array, size_t inSize, const wchar_t *outPath);
     EXPORT void ljd_bytes_to_file_append(HANDLE file, const wchar_t *name, const char *array, size_t inSize);
+#ifdef __cplusplus
 }
+#endif
